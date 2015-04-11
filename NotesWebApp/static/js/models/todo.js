076f7cf711181a10690a2b8b3744010b50e57fdd
Josh Castaneda <senterright@gmail.com>
2015-04-11 11:59:23 -0700

    Title: put fixtures
    Description:
    first fixtures for todo notes and non todo notes

--- NotesWebApp/static/js/models/todo.js
+++ NotesWebApp/static/js/models/todo.js
@@ -6,3 +6,32 @@ Todos.Todo = DS.Model.extend({
 Todos.Note = DS.Model.extend({
     msg: DS.attr('string')
 });
+
+Todos.Todo.FIXTURES = [
+{
+  id: 1,
+  title: 'First Todo note',
+  isCompleted: true 
+},
+{
+  id: 2,
+  title: '...',
+  isCompleted: false
+},
+{
+  id: 3,
+  title: 'Last note',
+  isCompleted: false
+}
+];
+
+Todos.Note.FIXTURES = [
+{
+  id: 4,
+  msg: 'First non Todo note, just a regular note.'
+},
+{
+  id: 5,
+  msg: 'Real test'
+}
+];
